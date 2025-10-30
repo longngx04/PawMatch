53b83766f2fe7d9e63769b652abc8acb34235f00
LongNguyen <longnguyen12080412@gmail.com>
2025-10-30 08:58:36 +0700

    signup controller added

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,42 +1,55 @@
-import User from '../models/user.model.js';
+
+import User from '../models/user.js';
 import bcrypt from 'bcryptjs';
-export const signup = async(req, res) =>{
-   const{fullname, email, password} = req.body;
+import { generateToken } from '../lib/ultils.js';
 
-   try{
-    if (!fullname || !email || !password) {
-        return res.status(400).json({ message: 'All fields are required' });
+export const signup = async (req, res) => {
+    const { fullname, email, password } = req.body;
+
+    try {
+        if (!fullname || !email || !password) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+        if (password.length < 6) {
+            return res.status(400).json({ message: 'Password must be at least 6 characters long' });
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: 'Invalid email format' });
+        }
+
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: 'Email already in use' });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            fullname,
+            email,
+            password: hashedPassword,
+        });
+
+        if (newUser) {
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullname: newUser.fullname,
+                email: newUser.email,
+                profilePicture: newUser.profilePicture,
+            });
+
+        }
+        else {
+            return res.status(400).json({ message: 'Invalid user data' });
+        }
+
+
+    } catch (error) {
+        console.log("Error in signup controller", error)
+        res.status(500).json({ message: 'Server error' });
     }
-    if (password.length < 6) {
-        return res.status(400).json({ message: 'Password must be at least 6 characters long' });
-    }
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        return res.status(400).json({ message: 'Invalid email format' });
-    }
-
-    const user = await User.findOne({ email });
-    if (user) {
-        return res.status(400).json({ message: 'Email already in use' });
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = new User({
-        fullname,
-        email,
-        password: hashedPassword,
-    });
-
-    if(newUser){
-        
-    }
-    else{
-        return res.status(400).json({ message: 'Invalid user data' });
-    }
-
-
-   } catch(error){
-       return res.status(500).json({ message: 'Internal server error' });
-   }
 } 
