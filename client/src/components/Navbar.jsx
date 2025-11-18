de407d733be4b1b1885ec21bcd046ee43275d16b
LongNguyen <longnguyen12080412@gmail.com>
2025-11-18 13:37:02 +0700

    Login and Sigup UI, HomePage Ui

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return (
+        <div className='bg-primary'>
+            <span className='text-white'>PawMatch</span>
+        </div>
+    )
+}
+
+export default Navbar
--- server/index.js
+++ server/index.js
@@ -3,10 +3,21 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './src/routes/auth.route.js'
 import {connectDB} from './src/lib/db.js'
-
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 dotenv.config()
 
 const app = express()
+
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.use(cookieParser());
+
+
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
